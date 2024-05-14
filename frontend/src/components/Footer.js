4c5aa2f794579dc5dcc637d9c8bae0e5ec964117
Abdullah Sinanović <budad-97@hotmail.com>
2024-05-15 01:56:24 +0200

    Fixed navigation and footer responsiveness for mobile and tablet

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,22 +14,22 @@ function Footer() {
     <div className="container footer-starter">
         <div className='row footer-row d-flex justify-content-center align-items-center'>
             <div className="col-12 footer-container d-flex justify-content-center align-items-center">
-                <Link to='/' className='col-1 d-flex justify-content-start align-items-center home-link'>
+                <Link to='/' className='col-1 home-link'>
                     <img src={Logo} alt="logo icon" className="footer-logo"/>
                 </Link>
-                <div className="design-container col-5 d-flex flex-column justify-content-start align-items-center">
-                    <h3 className="design col-12 d-flex justify-content-start align-items-center text-center p-0 m-0">Designed by: <span>Naghun</span></h3>
-                    <h3 className="rights col-12 d-flex justify-content-start align-items-center text-center p-0 m-0">All rights reserved <span>- 2024</span></h3>
+                <div className="design-container col-5">
+                    <h3 className="design col-12 p-0 m-0">Designed by: <span>Naghun</span></h3>
+                    <h3 className="rights col-12 p-0 m-0">All rights reserved <span>- 2024</span></h3>
                 </div>
-                <ul className="col-4 d-flex justify-content-center align-items-center footer-list">
+                <ul className="col-4 footer-list">
                     <li className="col-4 list-item"><Link to='/privacy-policy'>Privacy Policy</Link></li>
                     <li className="col-4 list-item"><Link to='/about'>About</Link></li>
                     <li className="col-4 list-item"><Link to='/contact'>Contact</Link></li>
                 </ul>
-                <div className="col-2 d-flex justify-content-end align-items-center footer-icons">
-                    <FontAwesomeIcon icon={faLinkedin} className="icon linkedin"/>
-                    <FontAwesomeIcon icon={faGithub} className="icon github"/>
-                    <FontAwesomeIcon icon={faEnvelope} className=" icon email"/>
+                <div className="col-2 footer-icons">
+                    <FontAwesomeIcon icon={faLinkedin} className="icon linkedin col-1"/>
+                    <FontAwesomeIcon icon={faGithub} className="icon github col-1"/>
+                    <FontAwesomeIcon icon={faEnvelope} className=" icon email col-1"/>
                 </div>
             </div>
         </div>
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -12,30 +12,30 @@ function Navigation() {
   return (
     <div className="container nav-starter">
         <div className='row navigation-row d-flex justify-content-center align-items-center'>
-            <nav class="col-12 d-flex justify-content-center align-items-center navbar navbar-expand-md navigation-container">
+            <nav className="col-12 d-flex justify-content-center align-items-center navbar navbar-expand-md navigation-container">
                 <Link to='/' onClick={() => handle_click('null')} className='navbar-brand col-4 d-flex justify-content-start align-items-center'>
                     <img src={Logo} alt="logo icon" className="nav-logo"/> Build Yourself
                 </Link>
-                <button class="navbar-toggler col-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler col-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse col-7" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto col-12 d-flex justify-content-end align-items-center">
-                    <li class="nav-item">
+                <div className="collapse navbar-collapse col-7" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto col-12 d-flex justify-content-end align-items-center">
+                        <li className="nav-item">
                             <Link to='/lobby' onClick={() => handle_click('lobby')} className={`nav-link ${active_link === 'lobby' ? 'active' : ''}`}>Lobby</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <Link to='/exercise' onClick={() => handle_click('exercise')} className={`nav-link ${active_link === 'exercise' ? 'active' : ''}`}>Exercise</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <Link to='/shop' onClick={() => handle_click('shop')} className={`nav-link ${active_link === 'shop' ? 'active' : ''}`}>Shop</Link>
                         </li>
-                        <li class="nav-item dropdown">
+                        <li className="nav-item dropdown">
                             <Link to='#' className='nav-link dropdown-toggle' id="navbarDropdown" 
                             role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Account
                             </Link>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <Link to="/exercise" className="dropdown-item">Exercise</Link>
                                 <Link to="/Shop" className="dropdown-item">Shop</Link>
                             </div>
